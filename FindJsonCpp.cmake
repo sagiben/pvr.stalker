13ae4707614641585199454232cc8042c8c1b766
Sagi <sagiben at gmail dot com>
2015-09-18 12:18:15 +0300

    better handling of FindJsoncpp

--- FindJsonCpp.cmake
+++ FindJsonCpp.cmake
@@ -3,13 +3,17 @@ if(PKG_CONFIG_FOUND)
   pkg_check_modules (JSONCPP jsoncpp)
 endif()
 
-if(NOT JSONCPP_FOUND)
-  find_path(JSONCPP_INCLUDE_DIRS json/json.h
-            PATH_SUFFIXES jsoncpp)
-  find_library(JSONCPP_LIBRARIES jsoncpp)
-endif()
+find_path(JSONCPP_INCLUDE_DIR
+  NAMES json/features.h
+  PATH_SUFFIXES jsoncpp
+  PATHS ${JSONCPP_PKGCONF_INCLUDE_DIRS}
+)
+
+find_library(JSONCPP_LIBRARY
+  NAMES jsoncpp
+  PATHS ${JSONCPP_PKGCONF_LIBRARY_DIRS}
+)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(JsonCpp DEFAULT_MSG JSONCPP_LIBRARIES JSONCPP_INCLUDE_DIRS)
-
-mark_as_advanced(JSONCPP_INCLUDE_DIRS JSONCPP_LIBRARIES)
+find_package_handle_standard_args(jsoncpp DEFAULT_MSG JSONCPP_LIBRARY JSONCPP_INCLUDE_DIR)
+mark_as_advanced(JSONCPP_INCLUDE_DIR JSONCPP_LIBRARY)
